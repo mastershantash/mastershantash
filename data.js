30faa90e0d8abc3c89ae01417fde8c3893d37cfc
Shantanu Sharma <shantash@cisco.com>
2020-10-31 01:13:54 +0530

    Amogh_1

---
+++ data.js
@@ -0,0 +1,87 @@
+data = {
+    "products": {
+        "phoenix" : {
+            "name":"Phoenix",
+            "images":[
+                "imgs/products/phoenix.png"
+            ],
+            "description":"Up to four 2” magnetron guns for confocal deposition on 2’’ wafers.",
+            "detail":"Phoenix is a UHV magnetron deposition system ideal for the research laboratories involved in thin-film research. With its four fixed angle (60o), confocal, 2-inch magnetron guns combined with a rotating z-axis substrate manipulator, the system is capable of depositing multilayer or alloy thin films. PID controlled substrate heating up to a temperature of 800 oC caters to most of the alloy deposition requirements. A UHV load lock system with a magnetically coupled substrate manipulator arm helps in loading and unloading the substrates without breaking the vacuum in the main chamber. The system has an optical port for in-situ laser interferometry-based stress measurements on the growing films. Additional ports for standard instruments like quartz crystal thickness monitors and electrical feed-throughs are also available.",
+            "features":[
+                "Automated system with remote control & monitoring of process parameters.",
+                "Up to four magnetron guns (2-inch) can be accommodated.",
+                "Shutters on the guns, substrate manipulator and view port.",
+                "Magnetically coupled rotation/translation motions.",
+                "Ports for in-situ measurements of stress and resistivity of films.",
+                "Easy maintenance of chamber with easy - remove OPAL-II sputtering guns." 
+            ]
+        },
+        "trinity" : {
+            "name":"Trinity",
+            "images":[
+                "imgs/products/trinity.png"
+            ],
+            "description":"Planetary configuration of three 1’’ or 2” magnetron guns for multilayer deposition on 1’’ wafers.",
+            "detail":"Trinity is an HV (1x10-7 mbar base pressure) low-cost magnetron deposition system ideal for the research laboratories, Universities, and Colleges The system is designed for housing three magnetron guns arranged in the planetary configuration. The system can accommodate both OPAL-I and OPAL-II range of magnetron sputtering guns. To keep the operation simple and costs in check, the system is fully manual controlled but can be automated if required. An HV load lock system with a magnetically coupled substrate manipulator provides easy substrate change capabilities without disturbing the vacuum of the main deposition chamber. Option for PID controlled substrate heating up to a temperature of 800 oC is available. The z-axis substrate manipulator can accommodate up to four different shadow masks. Easy in-situ mask alignment required between different layers of a device can be performed under a high vacuum. View port is provided above each magnetron guns for precise alignment of the substrate over the guns.",
+            "features":[
+                "Cost effective solution for magnetron sputtering based deposition.",
+                "Up to three magnetron guns (1-inch or 2-inch) can be accommodated.",
+                "Shutters on the guns, substrate manipulator and view port.",
+                "Magnetically coupled rotation/translation motions.",
+                "Easy maintenance of chamber with easy detach OPAL-I and OPAL-II sputtering guns."
+            ]
+        },
+        "opal-i" : {
+            "name":"Opal-I",
+            "images":[
+                "imgs/products/opal-I_1.png",
+                "imgs/products/opal-I_2.png"
+            ],
+            "description":"1” Magnetron Sputtering Guns ",
+            "detail":"OPAL-I is a 1” magnetron sputtering gun designed to operate in HV deposition chambers with pressure less than or equal to 10-7 mbar. The indirectly cooled permanent magnets are easy to replace and remain in direct contact with the target backing plate. FEM simulation guided field profiles provide excellent film uniformity over 1” substrate and more than 44% target utilization. The films with comparatively low surface roughness can be deposited at high deposition rates ~ 3-4 Å/s. Chimney with process gas feed ring allows stable operation from background pressures as high as 10-2 mbar to as low as 1x10-3 mbar.",
+            "features":[
+                "FEM simulated field profile provides excellent film uniformity.",
+                "High deposition rates of 3-4 Å/s.",
+                "Operable from 10-2 mbar to as low as 1x10-3 mbar background pressures.",
+                "Easy maintenance of magnets and gun.",
+                "Indirect cooling of magnets prevents magnet corrosion.",
+                "Hassel free target loading/unloading.",
+                "Excellent target utilization (~44%)."
+            ]
+        },
+        "opal-ii" : {
+            "name":"Opal-II",
+            "images":[
+                "imgs/products/opal-II_1.png",
+                "imgs/products/opal-II_2.png"
+            ],
+            "description":"2” Magnetron Sputtering Guns",
+            "detail":"OPAL-II is a 2” magnetron sputtering gun designed to operate in HV and UHV deposition chambers. The indirectly cooled permanent magnets are easy to replace and remain in direct contact of the target backing plate. FEM simulation guided field profiles provide excellent film uniformity over 2” substrate and more than 58% target utilization. The films with comparatively low surface roughness can be deposited at high deposition rates ~ 3-4 Å/s. Chimney with process gas feed ring allows stable operation from background pressures as high as 10-2 mbar to as low as 1x10-3 mbar.  Quick connect technology allows easy installation of guns in the deposition chamber.",
+            "features":[
+                "FEM simulated field profile provides excellent film uniformity.",
+                "High deposition rates of 3-4 Å/s.",
+                "Operable from 10-2 mbar to as low as 1x10-3 mbar background pressures.",
+                "Easy maintenance of magnets and gun.",
+                "Indirect cooling of magnets prevents magnet corrosion.",
+                "Hassel free target loading/unloading.",
+                "Excellent target utilization (~58%)."
+            ]
+        },
+        "amogh" : {
+            "name":"Amogh",
+            "images":[
+                "imgs/products/amogh_1.png",
+                "imgs/products/amogh_2.png"
+            ],
+            "description":"Mask-less Optical Lithography Setup",
+            "detail":"Amogh is a low-cost, mask-less optical lithography setup for thin-film device patterning. It is a direct laser writer capable of patterning any desired structure for sensor and device fabrication within ~1-10 mm feature size. The setup consists of a 405 nm diode laser focused with a binocular microscope. The mask patterns are generated using CAD software in PC and are fed to a microprocessor-controlled XY-raster stage that holds photo-resist coated substrates. The whole setup is built around open-source hardware and software for keeping the cost low.",
+            "features":[
+                "Direct laser lithography for patterning of thin films.",
+                "Remote control & monitoring of the pattern being generated.",
+                "Works with masks drawn in DXF format.",
+                "Manual XY and rotation stage for multiple layer alignment.",
+                "High pattern transfer rates of up to 1 mm2/s."
+            ]
+        }
+    }
+}
